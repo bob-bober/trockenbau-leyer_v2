496e67860a2a7a62831f76bb02beaa8d750df865
bob-bober <nigelkaya@gmail.com>
2026-01-17 18:23:14 +0100

    fixed navbar on other sites

--- components/PageTransition.js
+++ components/PageTransition.js
@@ -1,6 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
+import { gsap } from "gsap";
+
 export default function PageTransitionOverlays() {
+	useEffect(() => {
+		// Ensure overlays are positioned correctly on mount
+		gsap.set([".page-transition", ".page-transition-second"], { y: "-100%" });
+	}, []);
+
 	return (
 		<>
 			<div className="page-transition" aria-hidden="true" />
